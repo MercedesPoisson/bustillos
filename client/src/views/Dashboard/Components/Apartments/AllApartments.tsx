906916338df48a8587578d37e74e3438fc25cf90
MercedesPoisson <mercedespoisson@yahoo.com>
2023-09-26 17:37:41 -0300

    deleteApartment ok

--- client/src/views/Dashboard/Components/Apartments/AllApartments.tsx
+++ client/src/views/Dashboard/Components/Apartments/AllApartments.tsx
@@ -4,11 +4,15 @@ import getApartments from "../../../../redux/actions/getApartments";
 import { State } from "../../../../redux/Types";
 import { Apartments } from "../../../../redux/interfaces";
 import { BiEditAlt, BiTrash } from "react-icons/bi";
-import DeleteApartment from "./DeleteApartment/DeleteApartment";
+import Swal from "sweetalert2";
+import deleteApartments from "../../../../redux/actions/deleteApartment";
+import { AnyAction } from "redux";
 
 const AllApartments = () => {
   const apartments = useSelector((state: State) => state.apartments);
-  const [selectedApartment, setSelectedApartment] = useState<Apartments | null>(null);
+  const [selectedApartment, setSelectedApartment] = useState<Apartments | null>(
+    null
+  ); // eslint-disable-line @typescript-eslint/no-unused-vars
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false); // eslint-disable-line @typescript-eslint/no-unused-vars
   const dispatch = useDispatch();
 
@@ -24,14 +28,29 @@ const AllApartments = () => {
 
   const openDeleteModal = (apartment: Apartments) => {
     setSelectedApartment(apartment);
-    setIsDeleteModalOpen(true);
+    Swal.fire({
+      title: "¿Estás seguro/a que querés borrar este Departamento?",
+      text: "No podrás recuperarlo una vez que lo borres.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, borrar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Borrar el departamento seleccionado
+        dispatch(
+          deleteApartments(apartment.id_apartment) as unknown as AnyAction
+        );
+        Swal.fire(
+          "¡Departamento borrado!",
+          "Departamento borrado con éxito.",
+          "success"
+        );
+      }
+    });
   };
-
-  const closeDeleteModal = () => {
-    setSelectedApartment(null);
-    setIsDeleteModalOpen(false);
-  };
-
 
   return (
     <div className="bg-white px-4 pt-3 pb-4 rounded-sm border border-gray-200 flex-1">
@@ -53,49 +72,39 @@ const AllApartments = () => {
           </thead>
           <tbody>
             {apartments.map((apartment) => (
-              <tr key={apartment.id_apartment} className="text-center hover:bg-lightblue">
+              <tr
+                key={apartment.id_apartment}
+                className="text-center hover:bg-lightblue"
+              >
                 <td>{apartment.id_apartment}</td>
                 <td>{apartment.ap_number}</td>
                 <td>{apartment.title}</td>
                 <td>{apartment.price_per_night}</td>
                 <td>{apartment.min_nights}</td>
-                <td >{apartment.max_guests}</td>
+                <td>{apartment.max_guests}</td>
                 {/* <td>
                   {apartment.services.map((service) => (
                     <span key={service.id_service}>{service.name}</span>
                   ))}
                 </td> */}
                 <td className="text-center icon-cell">
-                    <button
-                    className="text-white bg-midblue hover:bg-blue px-1 py-1 rounded ml-4"
-                    >
-                        <BiEditAlt className="text-xl"  />
-                    </button>
-                  
+                  <button className="text-white bg-midblue hover:bg-blue px-1 py-1 rounded ml-4">
+                    <BiEditAlt className="text-xl" />
+                  </button>
                 </td>
                 <td className="text-center icon-cell">
-                <button
-                  className="text-white bg-red-500 hover:bg-red-600 px-1 py-1 rounded ml-4"
-                  onClick={() => openDeleteModal(apartment)}
-                >
-                  <BiTrash className="text-xl" />
-                </button>
+                  <button
+                    className="text-white bg-red-500 hover:bg-red-600 px-1 py-1 rounded ml-4"
+                    onClick={() => openDeleteModal(apartment)}
+                  >
+                    <BiTrash className="text-xl" />
+                  </button>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-      {isDeleteModalOpen && selectedApartment && (
-        <DeleteApartment
-          isOpen={true}
-          onClose={closeDeleteModal}
-          onDelete={() => {
-            closeDeleteModal();
-          }}
-          apartment={selectedApartment}
-        />
-      )}
     </div>
   );
 };
